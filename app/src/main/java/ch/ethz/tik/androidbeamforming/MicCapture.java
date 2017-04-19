8df8a0c1b3cbe27cac4c574a5158c72420acad47
Jonas Stehli <jonas.stehli@jsounds.ch>
2017-04-19 15:46:13 +0200

    19.04.2017 Code in MicCapture kopiert

--- app/src/main/java/ch/ethz/tik/androidbeamforming/MicCapture.java
+++ app/src/main/java/ch/ethz/tik/androidbeamforming/MicCapture.java
@@ -1,8 +1,51 @@
 package ch.ethz.tik.androidbeamforming;
 
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+
 /**
- * Created by Jonas Stehli on 4/19/2017.
+ * Created by Jonas Stehli in 2017.
+ * This class initiates a recorder.
+ * Audio Settings by default: 16 bit, 44.1 kHz
  */
 
 public class MicCapture {
+
+    private AudioRecord recorder = null;
+
+    private static final int SOURCE = MediaRecorder.AudioSource.MIC;
+    private static final int SAMPLERATE = 44100;
+    private static final int CHANNELS = AudioFormat.CHANNEL_IN_MONO;
+    private static final int ENCODING = AudioFormat.ENCODING_PCM_16BIT;
+    private static final int BUFFERSIZE = AudioRecord.getMinBufferSize(SAMPLERATE, CHANNELS, ENCODING);  // set buffersize as small as possible for minimum latency and calculate size in shorts for 16bit recording
+    private static final int SHORTSIZE = BUFFERSIZE / 2; // short array size for getting data is half the size of the buffer!!!
+
+    public int dataBit = 0;
+
+    public MicCapture() {
+
+        recorder = new AudioRecord(SOURCE, SAMPLERATE,
+                CHANNELS, ENCODING, BUFFERSIZE);
+
+    }
+
+    public void startRecording() {
+        recorder = new AudioRecord(SOURCE, SAMPLERATE,
+                CHANNELS, ENCODING, BUFFERSIZE);
+        recorder.startRecording();
+    }
+
+    public void stopRecording() {
+        recorder.stop();
+        recorder.release();
+        recorder = null;
+    }
+
+    public short[] getData() {
+        short[] data = new short[SHORTSIZE];
+        recorder.read(data, 0, SHORTSIZE);
+        return data;
+    }
+
 }
