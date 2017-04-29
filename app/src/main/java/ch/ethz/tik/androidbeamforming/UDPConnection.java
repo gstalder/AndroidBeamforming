965b7a1b829c163b482eab1de6dca178d5bc2534
Jonas Stehli <jonas.stehli@jsounds.ch>
2017-04-29 14:48:18 +0200

    UDPConnection implemented

--- app/src/main/java/ch/ethz/tik/androidbeamforming/UDPConnection.java
+++ app/src/main/java/ch/ethz/tik/androidbeamforming/UDPConnection.java
@@ -1,8 +1,43 @@
 package ch.ethz.tik.androidbeamforming;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
 /**
  * Created by Jonas Stehli on 4/29/2017.
  */
 
 public class UDPConnection {
+
+    DatagramSocket socket;
+    InetAddress localAddress;
+    InetAddress broadcastAddress;
+    int port = 0;
+
+    public void broadcast (String message) {
+
+        byte[] sendData = message.getBytes();
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, broadcastAddress, port);
+
+        try {
+            socket.setBroadcast(true);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            socket.send(sendPacket);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void send (String message, InetAddress host) {
+
+    }
+
 }
