f4db0dfd8a37739ebd21d222d4062c57496b14ba
gstalder <g.stalder26@gmail.com>
2017-05-18 11:37:28 +0200

    ClientAct

--- app/src/main/java/ch/ethz/tik/androidbeamforming/ClientActivity.java
+++ app/src/main/java/ch/ethz/tik/androidbeamforming/ClientActivity.java
@@ -218,7 +218,6 @@ public class ClientActivity extends AppCompatActivity {
                     }
                     micCaptureToSocket.start();
 
-                    //end of function
                     Log.d(TAG, " end of setAllConnections");
 
                 }catch(Exception e) {
